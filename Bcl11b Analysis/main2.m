f4eb3e59abbec262229faacd1c26c404cd802bda
KuehLabUW <kuehlab@uw.edu>
2018-11-02 14:34:07 -0700

    Remove duplicated directory

--- Bcl11b Analysis/main2.m
+++
@@ -1,9 +0,0 @@
-% script for running the 
-indir = {'/data/phnguyen/Imaging/RawData/082117_p21_EV_cMyc'};
-basenames = {'GoodExp'}
-outdir = '/data/phnguyen/Imaging/RawData/082117 - processed';
-
-% generate parameter file for image procesing.
-paramfile = 'params.mat';
-makeparams(paramfile);
-pipeline2(indir, basenames, outdir, paramfile);
